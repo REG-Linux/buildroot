82715f60f2641e09662253d2807120ab67b0e694
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-04 17:43:13 +0200

    Make bullet use double precision (needed for OpenMW)

--- package/bullet/bullet.mk
+++ package/bullet/bullet.mk
@@ -15,6 +15,9 @@ BULLET_LICENSE_FILES = LICENSE.txt
 # https://github.com/bulletphysics/bullet3/issues/3143
 BULLET_CONF_OPTS = -DBUILD_UNIT_TESTS=OFF
 
+# REG enable double precision for bullet
+BULLET_CONF_OPTS += -DUSE_DOUBLE_PRECISION=ON
+
 # extras needs dlfcn.h and NPTL (pthread_barrier_init)
 ifeq ($(BR2_STATIC_LIBS):$(BR2_TOOLCHAIN_HAS_THREADS_NPTL),:y)
 BULLET_CONF_OPTS += -DBUILD_EXTRAS=ON
