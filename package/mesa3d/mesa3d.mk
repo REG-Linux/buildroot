6c555317c81a1587fd650f05412fb5d99f854e05
Romain TISSERAND <romain.tisserand@gmail.com>
2024-08-29 18:12:48 +0200

    Bump Mesa3D to 24.2 series (24.2.1) and current + LTS 6.1/6.6 kernel

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # reglinux (update)
-MESA3D_VERSION = 24.1.6
+MESA3D_VERSION = 24.2.1
 MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://archive.mesa3d.org
 
