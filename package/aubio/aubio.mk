31c9c68ca17fd520bdf5675d0b6a1828f52e4c18
Juliano Dorigão <jdorigao@gmail.com>
2024-11-12 11:03:51 -0400

    2024.05.x: merge fix

--- package/aubio/aubio.mk
+++ package/aubio/aubio.mk
@@ -58,7 +58,7 @@ else
 AUBIO_CONF_OPTS += --disable-fftw3
 endif
 
-ifeq ($(BR2_PACKAGE_FFMPEG_SWRESAMPLE),y)
+ifeq ($(BR2_PACKAGE_FFMPEG),y)
 AUBIO_DEPENDENCIES += ffmpeg
 AUBIO_CONF_OPTS += --enable-avcodec
 else
--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,25 +4,16 @@
 #
 ################################################################################
 
-# REG: FFMPEG 6.0.1 OK
-# REG: rpi-FFMPEG 6.0 branch for amlogic
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_AMLOGIC_ANY),y)
-FFMPEG_VERSION = 9011d22fed1834cb7bd946349cc8a5eda748eec7
-FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.gz
-FFMPEG_SITE = https://github.com/jc-kynesim/rpi-ffmpeg
-FFMPEG_SITE_METHOD = git
-else
-FFMPEG_VERSION = 6.0.1
+FFMPEG_VERSION = 6.1.2
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.xz
 FFMPEG_SITE = https://ffmpeg.org/releases
-endif
 FFMPEG_INSTALL_STAGING = YES
 
 FFMPEG_LICENSE = LGPL-2.1+, libjpeg license
 FFMPEG_LICENSE_FILES = LICENSE.md COPYING.LGPLv2.1
 ifeq ($(BR2_PACKAGE_FFMPEG_GPL),y)
-FFMPEG_LICENSE += and GPL-3.0+
-FFMPEG_LICENSE_FILES += COPYING.GPLv3
+FFMPEG_LICENSE += and GPL-2.0+
+FFMPEG_LICENSE_FILES += COPYING.GPLv2
 endif
 
 FFMPEG_CPE_ID_VENDOR = ffmpeg
@@ -30,7 +21,7 @@ FFMPEG_CPE_ID_VENDOR = ffmpeg
 FFMPEG_CONF_OPTS = \
 	--prefix=/usr \
 	--enable-avfilter \
-	--enable-version3 \
+	--disable-version3 \
 	--enable-logging \
 	--enable-optimizations \
 	--disable-extra-warnings \
@@ -49,10 +40,6 @@ FFMPEG_CONF_OPTS = \
 	--disable-mipsdspr2 \
 	--disable-msa \
 	--enable-hwaccels \
-	--enable-dct \
-	--enable-fft \
-	--enable-mdct \
-	--enable-rdft \
 	--disable-cuda \
 	--disable-cuvid \
 	--disable-nvenc \
@@ -107,15 +94,9 @@ endif
 
 ifeq ($(BR2_PACKAGE_LIBV4L),y)
 FFMPEG_DEPENDENCIES += libv4l
-FFMPEG_CONF_OPTS += --enable-libv4l2 --enable-v4l2_m2m
+FFMPEG_CONF_OPTS += --enable-libv4l2
 else
 FFMPEG_CONF_OPTS += --disable-libv4l2
-endif
-
-ifeq ($(BR2_PACKAGE_FFMPEG_SWRESAMPLE),y)
-FFMPEG_CONF_OPTS += --enable-swresample
-else
-FFMPEG_CONF_OPTS += --disable-swresample
 endif
 
 ifeq ($(BR2_PACKAGE_FFMPEG_FFPROBE),y)
@@ -300,13 +281,17 @@ else
 FFMPEG_CONF_OPTS += --disable-vdpau
 endif
 
-
-# REG
-FFMPEG_CONF_OPTS += --enable-hwaccels
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+FFMPEG_CONF_OPTS += --enable-omx --enable-omx-rpi \
+	--extra-cflags=-I$(STAGING_DIR)/usr/include/IL
+FFMPEG_DEPENDENCIES += rpi-userland
+ifeq ($(BR2_arm),y)
+FFMPEG_CONF_OPTS += --enable-mmal
+else
+FFMPEG_CONF_OPTS += --disable-mmal
+endif
+else
 FFMPEG_CONF_OPTS += --disable-mmal --disable-omx --disable-omx-rpi
-#FFMPEG_CONF_OPTS += --enable-sand
-ifneq ($(BR2_PACKAGE_BATOCERA_TARGET_SAPHIRA)$(BR2_PACKAGE_BATOCERA_TARGET_SM8250)$(BR2_PACKAGE_BATOCERA_TARGET_BCM2835)$(BR2_PACKAGE_BATOCERA_TARGET_RK3128)$(BR2_PACKAGE_BATOCERA_TARGET_S812)$(BR2_PACKAGE_BATOCERA_TARGET_MT8395)$(BR2_mipsel)$(BR2_riscv)$(BR2_x86_64),y)
-FFMPEG_CONF_OPTS +=  --enable-libudev --enable-v4l2-request
 endif
 
 # To avoid a circular dependency only use opencv if opencv itself does
@@ -515,7 +500,9 @@ FFMPEG_CONF_OPTS += --enable-vfp
 else
 FFMPEG_CONF_OPTS += --disable-vfp
 endif
-ifeq ($(BR2_ARM_CPU_HAS_NEON)$(BR2_aarch64),y)
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+FFMPEG_CONF_OPTS += --enable-neon
+else ifeq ($(BR2_aarch64),y)
 FFMPEG_CONF_OPTS += --enable-neon
 else
 FFMPEG_CONF_OPTS += --disable-neon
@@ -542,6 +529,11 @@ else
 FFMPEG_CONF_OPTS += --disable-altivec
 endif
 
+# Fix build failure on several missing assembly instructions
+ifeq ($(BR2_RISCV_32),y)
+FFMPEG_CONF_OPTS += --disable-rvv --disable-asm
+endif
+
 # Uses __atomic_fetch_add_4
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
 FFMPEG_CONF_OPTS += --extra-libs=-latomic
@@ -566,6 +558,11 @@ endif
 FFMPEG_CFLAGS = $(TARGET_CFLAGS)
 
 ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_85180),y)
+FFMPEG_CONF_OPTS += --disable-optimizations
+FFMPEG_CFLAGS += -O0
+endif
+
+ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_68485),y)
 FFMPEG_CONF_OPTS += --disable-optimizations
 FFMPEG_CFLAGS += -O0
 endif
