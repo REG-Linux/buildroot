54cf11d2c3808daf0eea9f188bfc45399e157ba7
Juliano Dorigão <jdorigao@gmail.com>
2024-11-14 23:10:23 -0400

    update: 2024.02.08

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -16,8 +16,7 @@ XTERM_CONF_OPTS = --enable-256-color \
 	--x-libraries=$(STAGING_DIR)/usr/lib
 
 # Avoid freetype2 path poisoning by imake
-# batocera - add -ltinfo
-XTERM_CONF_ENV = ac_cv_path_IMAKE="" LIBS="-ltinfo"
+XTERM_CONF_ENV = ac_cv_path_IMAKE=""
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXFT),y)
 XTERM_DEPENDENCIES += xlib_libXft
