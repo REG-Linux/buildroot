579b52c831a50c574734fe4bed7bd8b356d57d50
Juliano Dorigão <jdorigao@gmail.com>
2024-11-22 09:52:03 -0400

    bump: rpi-firmware to 1.20241008

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -3,8 +3,8 @@
 # rpi-firmware
 #
 ################################################################################
-# reglinux (update) - Aligns to kernel version: 6.6.59
-RPI_FIRMWARE_VERSION = 0a3f48f2812ef52d3e83c0a008a347dd785ff153
+# reglinux (update)
+RPI_FIRMWARE_VERSION = 1.20241008
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
