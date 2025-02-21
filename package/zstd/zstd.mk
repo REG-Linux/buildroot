c8de9d4340776b8d109c2baa1a45872b6a7603fc
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-21 18:17:56 +0100

    Bump zstd to 1.5.7

--- package/zstd/zstd.mk
+++ package/zstd/zstd.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-ZSTD_VERSION = 1.5.6
+# REGLINUX bump (1.5.7 brings major performance improvements)
+ZSTD_VERSION = 1.5.7
 ZSTD_SITE = https://github.com/facebook/zstd/releases/download/v$(ZSTD_VERSION)
 ZSTD_INSTALL_STAGING = YES
 ZSTD_LICENSE = BSD-3-Clause or GPL-2.0
